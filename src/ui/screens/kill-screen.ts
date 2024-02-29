cfcba7e42f15714cda843243ec16d79d251f6509
Drew Harry <drew.harry@gmail.com>
2024-02-28 21:25:42 -0800

    changed killscreen language

--- src/ui/screens/kill-screen.ts
+++ src/ui/screens/kill-screen.ts
@@ -7,7 +7,7 @@ export class KillScreen extends Screen {
 
     draw(display: any, xOffset: number = 0, yOffset: number = 0) {
         display.drawText(10+xOffset, Math.floor(this.height/2) + yOffset,
-            "%c{#fff}%b{#a00}CONNECTION TERMINATED");
+            "%c{#fff}%b{#a00}RUNNER TERMINATED");
 
         display.drawText(12+xOffset, Math.floor(this.height/2)+3 + yOffset,
             "%c{#ccc}%b{#000}press any key to restart");
