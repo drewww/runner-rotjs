8fb49e0c20951f9155d9d8ced967815aaa7e2e11
Drew Harry <drew.harry@gmail.com>
2024-02-29 15:57:57 -0800

    discovered a neat visual effect for the title screen

--- src/ui/screens/title-screen.ts
+++ src/ui/screens/title-screen.ts
@@ -5,9 +5,11 @@ export class TitleScreen extends Screen {
     constructor() {
         super();
 
-        this.elements.push(new TextBox(10+this.x, Math.floor(this.height/2), 20, 5, "RUNNER", COLORS.WHITE, COLORS.BLACK, true, 0, 150));
-        this.elements.push(new TextBox(10+this.x, Math.floor(this.height/2)+1, 20, 5, "a cyberpunk escape roguelike", COLORS.WHITE, COLORS.BLACK, true, 150*6, 25));
-        this.elements.push(new TextBox(12+this.x, Math.floor(this.height/2)+3, 20, 5, "press any key to begin", COLORS.LIGHT_GREY, COLORS.BLACK, true, 150*6 + 50*28, 25));
+        this.elements.push(new TextBox(10+this.x, 5, 20, 5, "RUNNER", COLORS.WHITE, COLORS.BLACK, true, 0, 50));
+        this.elements.push(new TextBox(10+this.x, 6, 20, 5, "------", COLORS.WHITE, COLORS.WHITE, true, 0, 50));
+
+        this.elements.push(new TextBox(10+this.x, Math.floor(this.height/2)+3, 20, 5, "a cyberpunk escape roguelike", COLORS.WHITE, COLORS.BLACK, true, 50*6, 25));
+        this.elements.push(new TextBox(12+this.x, Math.floor(this.height/2)+5, 20, 5, "press any key to begin", COLORS.LIGHT_GREY, COLORS.BLACK, true, 150*6 + 50*28, 25));
     }
 
     public draw(display: any, xOffset: number = 0, yOffset: number = 0) {
