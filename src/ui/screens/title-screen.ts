d904e29bfd5e4c51846f8951968bf5e6ff1feabe
Drew Harry <drew.harry@gmail.com>
2024-02-29 17:14:17 -0800

    minor title screen timing tweak

--- src/ui/screens/title-screen.ts
+++ src/ui/screens/title-screen.ts
@@ -9,7 +9,7 @@ export class TitleScreen extends Screen {
         this.elements.push(new TextBox(10+this.x, 6, 20, 5, "------", COLORS.WHITE, COLORS.WHITE, true, 0, 50));
 
         this.elements.push(new TextBox(10+this.x, Math.floor(this.height/2)+3, 20, 5, "a cyberpunk escape roguelike", COLORS.WHITE, COLORS.BLACK, true, 50*6, 25));
-        this.elements.push(new TextBox(12+this.x, Math.floor(this.height/2)+5, 20, 5, "press any key to begin", COLORS.LIGHT_GREY, COLORS.BLACK, true, 150*6 + 50*28, 25));
+        this.elements.push(new TextBox(12+this.x, Math.floor(this.height/2)+5, 20, 5, "  press any key to begin", COLORS.LIGHT_GREY, COLORS.BLACK, true, 50*6, 25));
     }
 
     public draw(display: any, xOffset: number = 0, yOffset: number = 0) {
