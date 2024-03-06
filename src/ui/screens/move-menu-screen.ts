5e6d955d72c3f1c1f99215e9b55a71315c681699
Drew Harry <drew.harry@gmail.com>
2024-03-05 17:12:31 -0800

    Fixed background colors.

--- src/ui/screens/move-menu-screen.ts
+++ src/ui/screens/move-menu-screen.ts
@@ -10,18 +10,18 @@ export class MoveMenuScreen extends Screen {
         super(x,y); // passing 0,0 knowing that setPlayer will figure out the right values
         this.player = player;
         this.setPlayer(player);
-
-        this.elements!.push(new TextBox(0, 0, this.width, 1, "MOVES", COLORS.WHITE, COLORS.BLACK, false, 0, 50));    
     }
 
 
     draw(display: any, xOffset: number = 0, yOffset: number = 0) {
 
-        // for (let y = 0; y < height; y++) {
-        //     for (let x = 0; x < width; x++) {
-        //         display.draw(x + xOffset + this.x, y + yOffset + this.y, ' ', COLORS.LIGHT_GREY, COLORS.LIGHT_GREY);
-        //     }
-        // }
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                display.draw(x + xOffset + this.x, y + yOffset + this.y, '-', COLORS.DARK_GREY, COLORS.DARK_GREY);
+            }
+        }
+
+        display.drawText(xOffset + this.x, yOffset + this.y, `%c{${COLORS.BLACK}}%b{${COLORS.LIGHT_GREY}}MOVES%c{${COLORS.LIGHT_GREY}}%b{${COLORS.LIGHT_GREY}}${"-".repeat(this.width-5)}`)
 
         let i = 0;
         for (let move of this.player.moves) {
@@ -29,8 +29,8 @@ export class MoveMenuScreen extends Screen {
             const moveX = 1;
             const moveY = i + 1;
 
-            let bg = COLORS.BLACK;
-            let fg = COLORS.LIGHT_GREY;
+            let bg = COLORS.DARK_GREY;
+            let fg = COLORS.WHITE;
             if(move==this.player.getSelectedMove()) {
                 bg = COLORS.MOVE_BLUE;
                 fg = COLORS.WHITE;
